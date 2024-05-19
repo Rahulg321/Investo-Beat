570e7ba9a2b36b223ab764bf70d57d9062116e19
Rahul Gupta <rg5353070@gmail.com>
2024-05-19 16:50:41 +0530

    made changes to the newsletter slice

--- slices/Newsletter/index.tsx
+++ slices/Newsletter/index.tsx
@@ -11,37 +11,39 @@ export type NewsletterProps = SliceComponentProps<Content.NewsletterSlice>;
  * Component for "Newsletter" Slices.
  */
 const Newsletter = ({ slice }: NewsletterProps): JSX.Element => {
+  console.log(slice.primary.image);
+  const { image } = slice.primary;
+  const { width, height } = image.dimensions;
+
   return (
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
-      className="block-space-large "
+      className="block-space"
     >
       <div className="big-container flex flex-col border-8 md:flex-row">
-        <div className="aspect-h-1 aspect-w-1 relative basis-1/2 md:aspect-h-1 md:aspect-w-3 lg:top-[-80px]">
+        <div className="basis-1/3">
           <PrismicNextImage
             field={slice.primary.image}
-            fill
-            className="object-cover"
+            height={image.dimensions?.height}
+            width={image.dimensions?.width}
+            className=""
           />
         </div>
-        <div className="basis-1/2 p-4 md:px-6 md:py-12">
-          <div className="mb-4">
-            <h2 className="text-center">{slice.primary.heading}</h2>
-            <p className="">{slice.primary.tagline}</p>
-          </div>
-          <div className="">
-            <form action="flex ">
-              <input
-                type="text"
-                placeholder="Email Address"
-                className="bg-gray-200 p-2 md:p-4"
-              />
-              <button className="mt-4 border-2 border-mainO bg-white p-2 font-semibold text-mainO md:p-4">
-                Subscribe
-              </button>
-            </form>
-          </div>
+        <div className="basis-2/3 content-center space-y-2 p-4 text-center">
+          <h2 className="">{slice.primary.heading}</h2>
+          <p className="">{slice.primary.tagline}</p>
+
+          <form action="flex">
+            <input
+              type="text"
+              placeholder="Email Address"
+              className="bg-gray-200 p-2 md:p-4"
+            />
+            <button className="border-2 border-mainO bg-white p-2 font-semibold text-mainO md:p-4">
+              Subscribe
+            </button>
+          </form>
         </div>
       </div>
     </section>
